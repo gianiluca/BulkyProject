7638b040abfd7651896a59efe73b89708fdd5eb2
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2023-01-26 11:51:35 +0100

    Aggiunto il bottone per inserire una nuova categoria

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
