7ea0d36f4efe315b26ac6d84a3036760c1accbf0
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2023-02-08 11:56:16 +0100

    Delete Category - GET e POST

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -71,8 +71,38 @@ namespace BulkyBookWeb.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
+        }
 
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
 
         }
+
     }
 }
