9132370fdb6b3c95b727179759dc89e97131f48b
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2023-02-08 11:36:57 +0100

    Edit Category - POST

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -40,5 +40,39 @@ namespace BulkyBookWeb.Controllers
            
            
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(nameof(category.Name), $"The name of property {nameof(category.DisplayOrder)} cannot exactly match the name of property {nameof(category.Name)}");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+
+        }
     }
 }
