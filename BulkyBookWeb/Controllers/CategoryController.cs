0b3d89e3dd917668129032f40f9937cdaaa6e0ed
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2023-02-20 10:28:20 +0100

    Implementate IRepository e Repository

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -9,14 +9,17 @@ namespace BulkyBookWeb.Controllers
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
-            _db= db;
+            _db = db;
         }
+
+        //GET
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
 
+        //GET
         public IActionResult Create()
         {
             return View();
@@ -24,57 +27,58 @@ namespace BulkyBookWeb.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category category)
+        public IActionResult Create(Category obj)
         {
-            if (category.Name == category.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError(nameof(category.Name), $"The name of property {nameof(category.DisplayOrder)} cannot exactly match the name of property {nameof(category.Name)}");
+                ModelState.AddModelError(nameof(obj.Name), $"The name of property {nameof(obj.DisplayOrder)} cannot exactly match the name of property {nameof(obj.Name)}");
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(category);
+                _db.Categories.Add(obj);
                 _db.SaveChanges();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction(nameof(Index));
             }
-                return View(category); 
-           
-           
+            return View(obj);
+
         }
 
+        //GET
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
             {
                 return NotFound();
             }
-            //var categoryFirstOrDefault = _db.Categories.FirstOrDefault(x => x.Id == id);
-            var categoryFromDb = _db.Categories.Find(id);
-            if (categoryFromDb == null)
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFromDbFirst == null)
             {
                 return NotFound();
             }
-            return View(categoryFromDb);
+            return View(categoryFromDbFirst);
         }
-
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category category)
+        public IActionResult Edit(Category obj)
         {
-            if (category.Name == category.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError(nameof(category.Name), $"The name of property {nameof(category.DisplayOrder)} cannot exactly match the name of property {nameof(category.Name)}");
+                ModelState.AddModelError(nameof(obj.Name), $"The name of property {nameof(obj.DisplayOrder)} cannot exactly match the name of property {nameof(obj.Name)}");
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(category);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(obj);
+
         }
+
 
         //GET
         public IActionResult Delete(int? id)
@@ -83,30 +87,35 @@ namespace BulkyBookWeb.Controllers
             {
                 return NotFound();
             }
-            var categoryFromDb = _db.Categories.Find(id);
-            if (categoryFromDb == null)
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFromDbFirst == null)
             {
                 return NotFound();
             }
-            return View(categoryFromDb);
+            return View(categoryFromDbFirst);
         }
 
-
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public IActionResult DeletePost(int id)
+        public IActionResult DeletePost(int id, [Bind("Id")] Category category)
         {
-            var obj = _db.Categories.Find(id);
-            if (obj == null)
+            if (id != category.Id)
             {
                 return NotFound();
             }
-            _db.Categories.Remove(obj);
+            //var obj = _db.Categories.Find(id);
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDbFirst);
             _db.SaveChanges();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction(nameof(Index));
-
         }
-
     }
+
 }
+
