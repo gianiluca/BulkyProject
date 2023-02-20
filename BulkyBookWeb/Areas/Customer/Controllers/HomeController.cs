5e31f572ecc18db224c94991bd78a89fdcdb5faa
Gennaro Malafronte <gennaro.malafronte@gmail.com>
2023-02-20 11:49:48 +0100

    Aggiunte aree Admin e Customer

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BulkyBookWeb.Controllers;
+[Area("Customer")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+   pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 app.UseRequestLocalization("it-IT");
 app.Run();
